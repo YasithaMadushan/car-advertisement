b61ab12f1cab2f87555ef2dfa92190d3e117666e
YasithaMadushan <89100868+YasithaMadushan@users.noreply.github.com>
2024-04-29 17:35:56 +0530

    set fixed img size in details page

--- src/Components/CarDetailsPage.jsx
+++ src/Components/CarDetailsPage.jsx
@@ -9,7 +9,7 @@ const CarDetailsPage = () => {
   return (
     <div className="container">
       <h2>{car.model}</h2>
-      <img src={car.image} alt={car.model} />
+      <img src={car.image} alt={car.model} width='400px' />
       <p>{car.description}</p>
       <p>Price: {car.price}</p>
       <p>Year: {car.year}</p>
